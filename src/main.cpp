e749bfb0f389a69e6f7e43d31640c2ed6d9ce1dd
Ahmet Bugra Tohumcu <tohumcuahmetbugra@gmail.com>
2026-05-23 12:28:45 +0300

    fix: data file path gived as argument to the ui class

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,13 @@
 #include "ui/UserInterface.h"
+#include <iostream>
 
 int main() {
     // Instantiate orchestrator and spin up UI processing loop
-    mislib::UserInterface ui;
+
+    const std::string datafilePath = "books_dataset.txt";
+    mislib::UserInterface ui(datafilePath);
     ui.run();
+
 
     return 0;
 }
