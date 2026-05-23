cb777803df40f7c427f20f8143261851da3444f6
Ahmet Bugra Tohumcu <tohumcuahmetbugra@gmail.com>
2026-05-23 12:24:17 +0300

    refac: update method signature updated

--- src/persistence/CrudRepository.h
+++ src/persistence/CrudRepository.h
@@ -1,16 +1,16 @@
 #ifndef CRUD_REPO
 #define CRUD_REPO
 
-
+#include <cstdlib>
 namespace mislib
 {
     template<typename T>
     class CrudRepository{
     protected:
     virtual bool create( const T& data ) = 0;
-    // virtual bool get( size_t id, T& out ) = 0;
-    // virtual bool update( size_t id, T& newData ) = 0;
-    // virtual bool remove( size_t id ) = 0;
+    virtual bool get( std::size_t id, T& out ) = 0;
+    virtual bool update( const T& data ) = 0;
+    virtual bool remove( std::size_t id ) = 0;
 
     ~CrudRepository() = default;
 };
