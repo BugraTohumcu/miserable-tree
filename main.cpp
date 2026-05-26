67b215fdb291bd2d0908a3148cf306553eb13c51
Ahmet Bugra Tohumcu <tohumcuahmetbugra@gmail.com>
2026-05-26 16:23:11 +0300

    refac: main.cpp path updated

---
+++ main.cpp
@@ -0,0 +1,14 @@
+#include "src/ui/UserInterface.h"
+#include <iostream>
+
+int main() {
+    // Instantiate orchestrator and spin up UI processing loop
+
+    const std::string datafilePath = "books_dataset.txt";
+    mislib::UserInterface ui(datafilePath);
+    ui.run();
+
+
+    return 0;
+}
+    
